1b0698297b9a9a68df52e766d150e2d66dad2f84
alexsadiku1 <asadiku1@hotmail.com>
2016-12-06 14:34:46 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml;
+
+namespace ReadXMLfromURL
+{
+    /// <summary>
+    /// Summary description for Class1.
+    /// </summary>
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            String URLString = "http://opendata.dublincity.ie/TrafficOpenData/CP_TR/CPDATA.xml";
+            XmlTextReader reader = new XmlTextReader(URLString);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+
+                        while (reader.MoveToNextAttribute()) // Read the attributes.
+                            Console.Write(reader.Value + "  ");
+
+                        break;
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.WriteLine("");
+                        break;
+                }
+            }
+        }
+    }
+}
